a863825bad67ca814d4acd7fcf038810dea3f16c
IlyaFinkelshteyn <ilyaf@appveyor.com>
2017-01-19 13:08:14 -0800

    Initial checkin

---
+++ HelloWord.Tests.ps1
@@ -0,0 +1,27 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "HelloWord1" {
+    It "does something useful" {
+        $true | Should Be $true
+    }
+}
+
+Describe "HelloWord2" {
+    It "does something useful" {
+        $true | Should Be $true
+    }
+}
+
+Describe "HelloWord3" {
+    It "does something useful" {
+        $true | Should Be $true
+    }
+}
+
+Describe "HelloWord4" {
+    It "does something useful" {
+        $true | Should Be $true
+    }
+}
